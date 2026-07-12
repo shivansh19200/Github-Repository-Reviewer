49a539fb1ffb729e1c2906eb5f9c63c2be164500
shivansh19200 <67226327+shivansh19200@users.noreply.github.com>
2026-07-12 15:10:43 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,29 @@
+from typing import TypedDict
+
+from langgraph.graph import StateGraph
+
+from agents.architecture import architecture_agent
+from agents.code_quality_agent import code_quality_agent
+
+
+# This is the shared state that every agent can access
+class GraphState(TypedDict):
+
+    repo: str
+
+    architecture_review: str
+
+    code_quality_review: str
+
+builder = StateGraph(GraphState)
+
+
+builder.add_node("Architecture Reviewer", architecture_agent)
+builder.add_node("Code Quality Reviewer", code_quality_agent)
+
+builder.set_entry_point("Architecture Reviewer")
+
+
+builder.add_edge("Architecture Reviewer", "Code Quality Reviewer")
+
+graph = builder.compile()
